b1b2618560ef19b19507af335a254db42895b240
taliaperry <70407959+taliaperry@users.noreply.github.com>
2025-06-11 11:31:09 -0400

    Update vibrate.js
    Trying to fix good vibrations.

--- vibrate.js
+++ vibrate.js
@@ -1,5 +1,5 @@
 var isMobile = /iPhone|iPod|iPad|Android|BlackBerry/.test(navigator.userAgent)
  
-$('#CrackingOpenLogo').on(isMobile ? 'touchend' : 'click', function (e) {
+$('.buzz, a').on(isMobile ? 'touchend' : 'click', function (e) {
   navigator.vibrate(50)
 })
